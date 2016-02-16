28f6406c4b5ecc669f9abab93252aaedda99f24d
Peter Ivanov <peter@microweber.com>
2016-02-16 17:01:57 +0200

    upload to github

--- index.php
+++ index.php
@@ -13,31 +13,20 @@
 ?>
 <?php include TEMPLATE_DIR. "header.php"; ?>
 
-
-
-                  <div class="edit" field="content" rel="corp_content">
-                    <img src="<?php print template_url(); ?>img/home.png" class="element" alt="" />
-                        <h2 class="text-center">About us</h2>
-                        <hr style="margin-top: 0;">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
-                            Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
-                            purus tincidunt consequat a nec magna.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
-                            Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
-                            purus tincidunt consequat a nec magna.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
-                            Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
-                            purus tincidunt consequat a nec magna.
-                        </p>
-
-                </div>
-
+<div class="edit" field="content" rel="corp_content"> <img src="<?php print template_url(); ?>img/home.png" class="element" alt="" />
+  <h2 class="text-center">About us</h2>
+  <hr style="margin-top: 0;">
+  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
+    Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
+    purus tincidunt consequat a nec magna. </p>
+  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
+    Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
+    purus tincidunt consequat a nec magna. </p>
+  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas dapibus, massa commodo rutrum cursus, eros felis semper nulla, non tristique dui urna eget est.
+    Nullam tempus feugiat lacinia. Fusce tincidunt diam a scelerisque ullamcorper. Integer sit amet urna quis
+    purus tincidunt consequat a nec magna. </p>
+</div>
 <?php include TEMPLATE_DIR. "footer.php"; ?>
